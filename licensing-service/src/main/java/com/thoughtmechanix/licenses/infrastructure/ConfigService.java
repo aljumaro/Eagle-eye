41e5c79acd404614848840988f345df350a28b73
jalvrod <jalvrod@bahia97.bahia.net>
2017-01-23 13:35:32 +0100

    End of chapter 3: @RefreshScope

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/infrastructure/ConfigService.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/infrastructure/ConfigService.java
@@ -1,9 +1,11 @@
 package com.thoughtmechanix.licenses.infrastructure;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 @Service
+@RefreshScope
 public class ConfigService {
 	
 	@Value("${tracer.property}")
